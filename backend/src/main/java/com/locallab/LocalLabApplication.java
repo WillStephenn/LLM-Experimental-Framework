4b6210d35b431c7d6a6c623f1638fe6a253a42f2
William Stephen <zcabwst@ucl.ac.uk>
2025-11-27 18:44:02 +0000

    Initialise LocalLab project with basic spring-boot structure and configuration

---
+++ backend/src/main/java/com/locallab/LocalLabApplication.java
@@ -0,0 +1,13 @@
+package com.locallab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalLabApplication.class, args);
+	}
+
+}
